b99c010e0c04e06a725505ecd5440ae0daae67a3
Devin Mui <devinwmui@gmail.com>
2019-08-11 23:19:40 -0700

    Updated API + made callbacks smarter

--- Api.js
+++ Api.js
@@ -1,32 +1,53 @@
 class API {
 
-	static url = process.env['API_URL'] || 'http://localhost:3000'
+    static url = process.env['API_URL'] || 'http://localhost:3000'
 
-	static getScooters(cb){
-		cb(fetch(`${url}/scooters`))
-	}
+    static getScooters(cb){
+        fetch(`${url}/scooters`)
+        .then((res) => res.json())
+        .then((responseJson) => {
+            cb(null, responseJson)
+        })
+        .catch((err) => {
+            cb(err, null)
+        })
+    }
 
-	static startRide(req, cb){
-		if(!req.token) throw new Error('Stripe credit card `token` is required to start a ride')
-		if(!req.scooterId) throw new Error('`scooterId` is required to start a ride')
+    static startRide(req, cb){
+        if(!req.token) throw new Error('Stripe credit card `token` is required to start a ride')
+        if(!req.mac) throw new Error('`scooterId` is required to start a ride')
 
-		cb(fetch(`${url}/transactions/`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(req)
-		}))
-	}
+        fetch(`${url}/transactions/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(req)
+        })
+        .then((res) => res.json())
+        .then((responseJson) => {
+            cb(null, responseJson)
+        })
+        .catch((err) => {
+            cb(err, null)
+        })
+    }
 
-	static stopRide(req, cb){
-		if(!req.transactionId) throw new Error('`transactionId` is required to stop a ride')
+    static stopRide(req, cb){
+        if(!req.transactionId) throw new Error('`transactionId` is required to stop a ride')
 
-		cb(fetch(`${url}/transactions/${req.transactionId}`, {
-			method: 'PATCH',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		}))
-	}
+        fetch(`${url}/transactions/${req.transactionId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((responseJson) => {
+            cb(null, responseJson)
+        })
+        .catch((err) => {
+            cb(err, null)
+        })
+    }
 }
