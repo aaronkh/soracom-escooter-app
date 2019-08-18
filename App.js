f9028414dcd48367f4db8588792b05546c1f3b2a
aaron <huang.aaron03@gmail.com>
2019-08-18 01:46:08 -0700

    add splash screen and icon

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import {Provider } from 'react-redux'
-import {createStore} from 'redux'
-import {View} from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-import {reducer} from './redux/reducers/reducers'
+import Colors from './components/common/Colors'
+import { reducer } from './redux/reducers/reducers'
 import Navigator from './Navigator'
 
 const store = createStore(reducer)
 
-const App = () => <Provider store={store}><Navigator /></Provider>
+const App = () => <Provider store={store}><Navigator style={{ backgroundColor: Colors.backgroundColor }} /></Provider>
 
 export default App
--- views/Map.js
+++ views/Map.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react'
 import { View, PermissionsAndroid, Text } from 'react-native'
 import Geolocation from 'react-native-geolocation-service'
-import MapView, {Marker, Callout} from 'react-native-maps'
-import {connect} from 'react-redux'
+import MapView, { Marker, Callout } from 'react-native-maps'
+import { connect } from 'react-redux'
 import AsyncStorage from '@react-native-community/async-storage'
 
 import Colors from '../components/common/Colors'
@@ -14,17 +14,17 @@ import UserLocation from '../components/Map/UserLocation'
 import ScooterPreview from '../components/Map/ScooterPreview'
 import QRCodeScanner from '../components/QRCodeScanner/QRCodeScanner'
 import ActiveScooter from '../components/ActivsScooter/ActiveScooter'
-import {switchCCToken} from '../redux/actions/actions'
+import { switchCCToken } from '../redux/actions/actions'
 
-import {distanceBetween} from '../util/DistanceCalculator'
+import { distanceBetween } from '../util/DistanceCalculator'
 
 const MAP_ANIMATION_DURATION = 888
-const FAKE_SCOOTER_LOCATIONS =[ 
+const FAKE_SCOOTER_LOCATIONS = [
     { _id: 0, longitude: -122.5022639462199, latitude: 37.76872868979763 },
     { _id: 1, longitude: -122.4929547979371, latitude: 37.78290650705166 },
     { _id: 2, longitude: -122.501239215147, latitude: 37.77325481238279 },
     { _id: 3, longitude: -122.48919211437241, latitude: 37.781040306887824 },
-    { _id: 4, longitude: -122.50502787240144, latitude: 37.77288140696781 } ]
+    { _id: 4, longitude: -122.50502787240144, latitude: 37.77288140696781 }]
 
 class Map extends React.Component {
     constructor(props) {
@@ -42,34 +42,34 @@ class Map extends React.Component {
 
     recenterMap = () => {
         this.refs["mapview"].animateToRegion(
-            { 
-                latitude: this.state.lat, 
-                longitude: this.state.lng, 
+            {
+                latitude: this.state.lat,
+                longitude: this.state.lng,
                 longitudeDelta: 0.001,
                 latitudeDelta: 0.01
-            }, 
+            },
             MAP_ANIMATION_DURATION)
-        this.setState({activeScooter: false})
+        this.setState({ activeScooter: false })
     }
 
     openQRScanner = () => {
         if (!this.state.gpsActive || !this.state.permissionGPS) return
-        this.setState({QRScannerOpen: true})
+        this.setState({ QRScannerOpen: true })
     }
 
-    closeQRScanner = () => this.setState({QRScannerOpen: false})
+    closeQRScanner = () => this.setState({ QRScannerOpen: false })
 
     updatePosition = () => {
         Geolocation.watchPosition(
             (position) => {
-                if(this.state.firstLocation) 
+                if (this.state.firstLocation)
                     this.refs["mapview"].animateToRegion(
-                        { 
-                            latitude: position.coords.latitude, 
-                            longitude: position.coords.longitude, 
+                        {
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude,
                             longitudeDelta: 0.001,
                             latitudeDelta: 0.01
-                        }, 
+                        },
                         MAP_ANIMATION_DURATION)
                 this.setState({
                     lat: position.coords.latitude,
@@ -111,9 +111,9 @@ class Map extends React.Component {
     findScooter = async (location) => {
         let lat = location.nativeEvent.coordinate.latitude
         let lng = location.nativeEvent.coordinate.longitude
-        await FAKE_SCOOTER_LOCATIONS.map(i=>{
-            if(i.longitude == lng && i.latitude == lat) {
-                this.setState({activeScooter: i})
+        await FAKE_SCOOTER_LOCATIONS.map(i => {
+            if (i.longitude == lng && i.latitude == lat) {
+                this.setState({ activeScooter: i })
             }
         })
     }
@@ -147,33 +147,33 @@ class Map extends React.Component {
                     }}
                     style={{ flex: 1 }}
                 >
-                    <Marker coordinate={{latitude: this.state.lat, longitude: this.state.lng}}>
-                    <UserLocation/>
+                    <Marker coordinate={{ latitude: this.state.lat, longitude: this.state.lng }}>
+                        <UserLocation />
                     </Marker>
                     {
-                        FAKE_SCOOTER_LOCATIONS.map(i => 
-                        <Marker onPress={this.findScooter} key={i._id} coordinate={i} title={`Scooter #` + i._id} pinColor={'teal'} />)
+                        FAKE_SCOOTER_LOCATIONS.map(i =>
+                            <Marker onPress={this.findScooter} key={i._id} coordinate={i} title={`Scooter #` + i._id} pinColor={'teal'} />)
                     }
                 </MapView>
-                
+
                 <View style={{ position: 'absolute', alignItems: 'flex-end', bottom: 18, right: 12 }}>
                     <CenterButton onPress={this.recenterMap} />
-                    <Spacer height={8}/>
+                    <Spacer height={8} />
                     <QRScanButton onPress={this.openQRScanner} />
                 </View>
-                {this.state.activeScooter &&  
-                <ScooterPreview 
-                    name={'Scooter #'+(this.state.activeScooter._id + 1)}
-                    distance={distanceBetween(this.state.lat, this.state.lng, this.state.activeScooter.latitude, this.state.activeScooter.longitude)}
-                    price="0.21"
-                    battery="50"
-                />}
-               {this.state.QRScannerOpen &&
-                <QRCodeScanner
-                    close={this.closeQRScanner}
-                />}
-                {this.props.ccToken? <ActiveScooter/> :
-                    <View/>
+                {this.state.activeScooter &&
+                    <ScooterPreview
+                        name={'Scooter #' + (this.state.activeScooter._id + 1)}
+                        distance={distanceBetween(this.state.lat, this.state.lng, this.state.activeScooter.latitude, this.state.activeScooter.longitude)}
+                        price="0.21"
+                        battery="50"
+                    />}
+                {this.state.QRScannerOpen &&
+                    <QRCodeScanner
+                        close={this.closeQRScanner}
+                    />}
+                {this.props.ccToken ? <ActiveScooter /> :
+                    <View />
                 }
             </Fragment>
         )
@@ -181,11 +181,11 @@ class Map extends React.Component {
 }
 
 function mapStateToProps(state) {
-    return {ccToken: state.ccToken}
+    return { ccToken: state.ccToken }
 }
 
 function mapDispatchToProps(dispatch) {
-    return {switchCCToken: val => dispatch(switchCCToken(val))}
+    return { switchCCToken: val => dispatch(switchCCToken(val)) }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Map)
